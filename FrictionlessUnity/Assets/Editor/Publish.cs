5817b8dbf8c0ab7ba7b6957d957908e7a55afa23
Daniel Wilkins <daniel.wilkins@scopely.com>
2019-03-06 17:32:20 -0800

    Fix exporting of Frictionless package

--- FrictionlessUnity/Assets/Editor/Publish.cs
+++ FrictionlessUnity/Assets/Editor/Publish.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using System.Collections;
+using System.Linq;
 
 public static class Publish
 {
-	[MenuItem("Frictionless/Publish Package")]
-	public static void PublishPackage()
-	{
-		const string exportFilename = "../Frictionless.unitypackage";
-		AssetDatabase.ExportPackage("Assets/Frictionless", exportFilename, ExportPackageOptions.Default);
-		Debug.Log ("Exported " + exportFilename);
-	}
+    [MenuItem("Frictionless/Publish Package")]
+    public static void PublishPackage()
+    {
+        const string exportFilename = "../Frictionless.unitypackage";
+
+        // As the project now contains .asmdef files, we want to ensure only scripts get exported
+        var assetGuids = AssetDatabase.FindAssets("t:script", new[] {"Assets/Frictionless"});
+
+        AssetDatabase.ExportPackage(assetGuids.Select(asset => AssetDatabase.GUIDToAssetPath(asset)).ToArray(),
+            exportFilename, ExportPackageOptions.Recurse);
+        Debug.Log("Exported " + exportFilename);
+    }
 }
